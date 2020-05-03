b78dfac87cd1baee3e47eae543035a35fac448e4
github katzeallergie <saito.t.av@m.titech.ac.jp>
2020-05-03 18:13:09 +0900

    Style MemoDetailScreen

--- src/elements/CircleButton.js
+++ src/elements/CircleButton.js
@@ -3,9 +3,19 @@ import {StyleSheet, View, Text} from "react-native";
 
 class CircleButton extends React.Component {
   render() {
+    const { style, color } = this.props;
+
+    let bgColor = "#E31676";
+    let textColor = "#fff";
+
+    if (color === "white") {
+      bgColor = "#fff";
+      textColor = "#E31676";
+    }
+
     return(
-      <View style={styles.CircleButton}>
-        <Text style={styles.CircleButtonitle}>
+      <View style={[styles.CircleButton, style, { backgroundColor: bgColor}]}>
+        <Text style={[styles.CircleButtonitle, {color: textColor}]}>
           {this.props.children}
         </Text>
       </View>
@@ -20,7 +30,6 @@ const styles = StyleSheet.create({
     right: 36,
     width: 48,
     height: 48,
-    backgroundColor: "#E31676",
     borderRadius: 24,
     justifyContent: "center",
     alignItems: "center",
@@ -32,7 +41,6 @@ const styles = StyleSheet.create({
   CircleButtonitle: {
     fontSize: 24,
     lineHeight: 24,
-    color: "#FFF"
   },
 });
 
